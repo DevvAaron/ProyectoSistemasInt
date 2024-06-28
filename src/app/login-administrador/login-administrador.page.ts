2ae7477e5f56eab77e642c753e99a2fa3eb5299a
gersonzeta <gezetab@ucvvirtual.edu.pe>
2024-06-27 23:27:58 -0500

    Diseño SemiFinal
    Arreglos del diseño

--- src/app/login-administrador/login-administrador.page.ts
+++ src/app/login-administrador/login-administrador.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { LoginAdministradorService } from './login-administrador.service';
 
 @Component({
@@ -8,17 +9,20 @@ import { LoginAdministradorService } from './login-administrador.service';
   styleUrls: ['./login-administrador.page.scss'],
 })
 export class LoginAdministradorPage implements OnInit {
-  usuarios: any[] = [];
-  labs: any[] = [];
-  clips: any[] = [];
+  formAdministrador: FormGroup;
+  usuarios: any[] = []; // Declaración de la propiedad usuarios
 
-  id: string = '';
-  usuario: string = '';
-  clave: string = '';
-  correo: string = '';
-
-  constructor(private navCtrl: NavController,
-    private loginAdministradorService: LoginAdministradorService) { }
+  constructor(
+    private navCtrl: NavController,
+    public fb: FormBuilder,
+    private loginAdministradorService: LoginAdministradorService
+  ) {
+    this.formAdministrador = this.fb.group({
+      'usuario': new FormControl('', Validators.required),
+      'clave': new FormControl('', Validators.required),
+      'correo': new FormControl('', [Validators.required, Validators.email])
+    });
+  }
 
   ngOnInit() {
     this.cargarUsuarios();
@@ -27,26 +31,25 @@ export class LoginAdministradorPage implements OnInit {
   cargarUsuarios() {
     this.loginAdministradorService.getUsuarios().subscribe(data => {
       console.log('Usuarios:', data);
-      this.usuarios = data;
+      this.usuarios = data; // Asignación de datos a la propiedad usuarios
     });
   }
 
   registrarUsuario() {
-    const newUsuario = {
-      Usuario: this.usuario,
-      Clave: this.clave,
-      Correo: this.correo
-    };
+    if (this.formAdministrador.valid) {
+      const { usuario, clave, correo } = this.formAdministrador.value;
+      const newUsuario = { Usuario: usuario, Clave: clave, Correo: correo };
   
-    this.loginAdministradorService.registrarUsuario(newUsuario).subscribe(res => {
-      console.log('Respuesta del backend:', res);
-      // Actualizar la lista después de registrar
-      this.cargarUsuarios();
-    });
+      this.loginAdministradorService.registrarUsuario(newUsuario).subscribe(res => {
+        console.log('Respuesta del backend:', res);
+        this.cargarUsuarios(); // Actualización de la lista después de registrar
+      });
+    } else {
+      alert('Por favor, complete todos los campos correctamente.');
+    }
   }
   
-  async regresarHome(){
-  
-  this.navCtrl.navigateBack('/home');
-}
+  async regresarHome() {
+    this.navCtrl.navigateBack('/home');
+  }
 }
